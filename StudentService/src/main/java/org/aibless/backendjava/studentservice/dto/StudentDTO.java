61bc84b5b1a4d1e11acb1e659b8099dffad7d5e6
LMThumeo <nilelouis2611@gmail.com>
2021-07-12 07:41:18 +0700

    use lombok instead get/set

--- StudentService/src/main/java/org/aibless/backendjava/studentservice/dto/StudentDTO.java
+++ StudentService/src/main/java/org/aibless/backendjava/studentservice/dto/StudentDTO.java
@@ -1,7 +1,10 @@
 package org.aibless.backendjava.studentservice.dto;
 
+import lombok.Data;
+
 import java.sql.Date;
 
+@Data
 public class StudentDTO {
 
     private Integer id;
@@ -13,44 +16,4 @@ public class StudentDTO {
     private String address;
 
     private Date dob;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Date getDob() {
-        return dob;
-    }
-
-    public void setDob(Date dob) {
-        this.dob = dob;
-    }
 }
